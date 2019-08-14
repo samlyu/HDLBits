c09609aeecf6ed8632a41551e7b6d6d767faef54
Shengwei Lyu <samlyu@users.noreply.github.com>
2019-08-14 13:38:57 +0800

    Update 19_serialdp.v

--- 2_circuits/2_sequential/fsm/19_serialdp.v
+++ 2_circuits/2_sequential/fsm/19_serialdp.v
@@ -4,14 +4,16 @@ module top_module(
     input reset,    // Synchronous reset
     output [7:0] out_byte,
     output done
-); //
+); 
 
     // Modify FSM and datapath from Fsm_serialdata
     parameter idle = 4'd0, start = 4'd1, trans0 = 4'd2, trans1 = 4'd3, trans2 = 4'd4, trans3 = 4'd5;
     parameter trans4 = 4'd6, trans5 = 4'd7, trans6 = 4'd8, trans7 = 4'd9, stop = 4'd10, error = 4'd11, pari = 4'd12;
     reg [3:0]   state, next_state;
-    reg [8:0]   data;
-    
+    reg [7:0]   data;
+    wire        odd, reset_p;
+    reg         done_reg;
+
     always@(*) begin
         case(state)
             idle: begin
@@ -30,16 +32,10 @@ module top_module(
             trans6: next_state = trans7;
             trans7: next_state = pari;
             pari: begin
-                if(in && ^data==1'b1)
-                    next_state = stop;
-                else
-                    next_state = error;
-            end
-            stop: begin
                 if(in)
                     next_state = idle;
                 else
-                    next_state = start;
+                    next_state = error;
             end
             error: begin
                 if(in)
@@ -56,22 +52,30 @@ module top_module(
         else
             state <= next_state;
     end
-    
-    assign  done = (state == stop);
 
     always@(posedge clk) begin
-        if(reset)
-            data <= 9'd0;
+        if(reset) begin
+            data <= 8'd0;
+            reset_p <= 1'b1;
+            done_reg <= 1'b0;
+        end
         else begin
             if(next_state == trans0 || next_state == trans1 || next_state == trans2 || next_state == trans3 || next_state == trans4 || next_state == trans5 || next_state == trans6 || next_state == trans7)
-                data <= {in, data[8:1]}; // LSB first
-            else if(next_state == stop || next_state == pari)
-                data <= data;
-            else
-                data <= 9'd0;
+                data <= {in, data[7:1]}; // LSB first
+            else if(next_state == start) begin
+                data <= 8'd0;
+                reset_p <= 1'b0;
+                done_reg <= 1'b0;
+            end  
+            else if(next_state == idle)
+                done_reg <= odd;
+            else if(next_state == pari)
+                reset_p <= 1'b1;
         end
     end
-    
-    assign  out_byte = (state == stop)?data[7:0]:8'd0;
-    
+
+    assign  done = done_reg;    
+    assign  out_byte = done?data:8'd0;
+    parity par_mod(clk, reset|reset_p, in, odd);
+
 endmodule

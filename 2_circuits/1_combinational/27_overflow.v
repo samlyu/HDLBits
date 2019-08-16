1ac5eb737702d4ab14560526ae7e52563c858d79
Shengwei Lyu <samlyu@users.noreply.github.com>
2019-08-16 08:39:55 +0800

    Update 27_overflow.v

--- 2_circuits/1_combinational/27_overflow.v
+++ 2_circuits/1_combinational/27_overflow.v
@@ -5,7 +5,18 @@ module top_module (
     output overflow
 ); //
  
+    // method 01
     assign	s = a+b;
     assign	overflow = (a[7]==b[7] && a[7]!=s[7]);
 
+    // method 02
+    //assign	overflow = (~a[7])&(~b[7])&(c) | a[7]&b[7]&(~c);
+    
+    // method 03
+    //wire [6:0]	dumb;
+    //wire		c2, c;
+    //assign	{c2,s} = a + b;
+    //assign	{c, dumb} = a[6:0] + b[6:0];
+    //assign	overflow = c2 ^ c;
+    
 endmodule
